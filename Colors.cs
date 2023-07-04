fda07b224be12a3b0a694fc4992d51affee0e7d3
CeriiZedF <dasbalan14@gmail.com>
2023-07-04 17:20:17 +0300

    Добавьте файлы проекта.

---
+++ Colors.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorViever
+{
+    internal class Colors
+    {
+        public byte alpha { get; set; }
+        public byte red { get; set; }
+        public byte green { get; set; }
+        public byte blue { get; set; }
+
+
+        public Colors() { }
+
+    }
+}
---
+++ VievModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace ColorViever
+{
+    internal class VievModel : INotifyPropertyChanged
+    {
+        public Colors MyColor;
+       
+        public VievModel() 
+        {
+            MyColor = new Colors();
+            UpdateBackground();
+        }
+
+        public byte Alpha
+        {
+            get => MyColor.alpha;
+            set
+            {
+                MyColor.alpha = value;
+                OnPropertyChanged("Alpha");
+            }
+        }
+        public byte Red
+        {
+            get => MyColor.red;
+            set
+            {
+                MyColor.red = value;
+                OnPropertyChanged("Red");
+            }
+        }
+
+        public byte Green
+        { 
+            get => MyColor.green;
+            set
+            {
+                MyColor.green = value;
+                OnPropertyChanged("Green");
+            }
+        }
+        public byte Blue
+        {
+            get => MyColor.blue;
+            set
+            {
+                MyColor.blue = value;
+                OnPropertyChanged("Blue");
+            }
+        }
+        public SolidColorBrush UpdateBackground()
+        {
+            return new SolidColorBrush(System.Windows.Media.Color.FromArgb(Alpha, Red, Green, Blue));
+            
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                this.PropertyChanged(this, e);
+            }
+        }
+    }
+}
